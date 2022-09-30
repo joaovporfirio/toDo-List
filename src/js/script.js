fec0a1ba83d7e588558aa530870d9726a3d685b0
João Victor Porfírio <108318270+joaovporfirio@users.noreply.github.com>
2022-09-30 12:47:59 -0300

    Todo List

--- src/js/script.js
+++ src/js/script.js
@@ -6,9 +6,9 @@ var toDoList = document.querySelector('#todo-list')
 var editForm = document.querySelector('#edit-form')
 var editInput = document.querySelector('#edit-input')
 var cancelEditBtn = document.querySelector('#cancel-edit-btn')
+var eraseButton = document.querySelector('#erase-button')
 
-
-
+let oldInputValue
 
 //Funções
 
@@ -27,7 +27,7 @@ const saveToDo = (text) => {
     todo.appendChild(doneBtn)
 
     const editToDo = document.createElement("button")
-    editToDo.classList.add("finish-todo")
+    editToDo.classList.add("edit-todo")
     editToDo.innerHTML = '<i class="fa-sharp fa-solid fa-pen"></i>'
     todo.appendChild(editToDo)
 
@@ -47,10 +47,21 @@ const toggleforms = () => {
     editForm.classList.toggle("hide")
     toDoForm.classList.toggle("hide")
     toDoList.classList.toggle("hide")
-} 
+}
 
+const updateTodo = (text) => {
 
+    const todos = document.querySelectorAll(".todo")
 
+    todos.forEach((todo) => {
+
+        let todoTitle = todo.querySelector("h3")
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text
+        }
+    })
+}
 
 //Eventos
 
@@ -61,9 +72,6 @@ toDoForm.addEventListener("submit", (e) => {
     var texto = toDoInput.value
     if (texto) {
         saveToDo(texto)
-
-
-
     }
 })
 
@@ -71,19 +79,25 @@ document.addEventListener("click", (e) => {
 
     const targetEl = e.target
     const parentEl = targetEl.closest("div")
+    let todoTitle
+
+    if (parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText
+    }
 
     if (targetEl.classList.contains("finish-todo")) {
         parentEl.classList.toggle("done")
     }
 
-    if(targetEl.classList.contains("remove-todo")) {
+    if (targetEl.classList.contains("remove-todo")) {
         parentEl.remove()
     }
 
-    if(targetEl.classList.contains("edit-todo")){
+    if (targetEl.classList.contains("edit-todo")) {
         toggleforms()
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
     }
-
 })
 
 cancelEditBtn.addEventListener("click", (e) => {
@@ -91,3 +105,95 @@ cancelEditBtn.addEventListener("click", (e) => {
     toggleforms()
 
 })
+
+
+editForm.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+    const editInputValue = editInput.value
+
+    if (editInputValue) {
+        updateTodo(editInputValue)
+
+    }
+    toggleforms()
+})
+
+
+
+// Criando filtro de busca (finalizado ou em aberto)
+
+const filterselect = document.querySelector('#filter-select  ')
+filterselect.addEventListener("change", acao => {
+
+    if (filterselect.value == 'todo') {
+        arrays = Array.from(toDoList.children).filter(todo =>
+            todo.classList.contains('done')).forEach(todo => {
+
+                todo.classList.add('hidden')
+
+            })
+
+        arrays = Array.from(toDoList.children).filter(todo =>
+            !todo.classList.contains('done')).forEach(todo => {
+
+                todo.classList.remove('hidden')
+            })
+
+            
+    }
+
+
+    if (filterselect.value == 'all') {
+        arrays = Array.from(toDoList.children).filter(todo =>
+            todo.classList.contains('hidden')).forEach(todo => {
+
+                todo.classList.remove('hidden')
+            })
+
+    }
+
+    if (filterselect.value == 'done') {
+        arrays = Array.from(toDoList.children).filter(todo =>
+            !todo.classList.contains('done')).forEach(todo => {
+
+                todo.classList.add('hidden')
+            })
+
+        arrays = Array.from(toDoList.children).filter(todo =>
+            todo.classList.contains('done')).forEach(todo => {
+
+                todo.classList.remove('hidden')
+            })
+
+    }
+
+})
+
+
+// Filtro de busca
+const searchInput = document.querySelector('#search input')
+searchInput.addEventListener('input', event => {
+    const inputsearchvalue = event.target.value.trim().toLowerCase()
+    filteredsearch = Array.from(toDoList.children).filter(todo =>
+        !todo.textContent.toLowerCase().includes(inputsearchvalue)).forEach(todo => {
+            todo.classList.add('hidden')
+        })
+    Array.from(toDoList.children).filter(todo =>
+        todo.textContent.toLowerCase().includes(inputsearchvalue)).forEach(todo => {
+            todo.classList.remove('hidden')
+        })
+
+    eraseButton.addEventListener('click', (e) => {
+
+        e.preventDefault()
+        const inputsearcheraser = document.querySelector('#search-input')
+        inputsearcheraser.value = ''
+        Array.from(toDoList.children).filter(todo =>
+            todo.textContent.toLowerCase().includes(inputsearchvalue)).forEach(todo => {
+                todo.classList.remove('hidden')
+            })
+    })
+
+})
+
